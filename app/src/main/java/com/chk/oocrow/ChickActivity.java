ccc6dcf085275a3062994867ffebdc9e8add352a
liujinchao <liujinchao@gionee.com>
2017-12-28 09:53:12 +0800

    utils

--- app/src/main/java/com/chk/oocrow/ChickActivity.java
+++ app/src/main/java/com/chk/oocrow/ChickActivity.java
@@ -1,13 +1,27 @@
 package com.chk.oocrow;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.SeekBar;
 
 public class ChickActivity extends AppCompatActivity {
 
     MyChickView mMyChickView;
     SeekBar mSeekBar;
+    Utils mUtils;
+
+    private float defaultVolume;
+    private AudioManager mAudioManager;
+    private Handler mHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +30,24 @@ public class ChickActivity extends AppCompatActivity {
         init();
     }
 
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                defaultVolume = mUtils.getVolume();
+                break;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                defaultVolume = mUtils.getVolume();
+                Log.d("asd", "defaultVolume: " + String.valueOf(defaultVolume));
+                break;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
     void init() {
-        viewInit();
         dataInit();
+        viewInit();
     }
 
     void viewInit() {
@@ -28,6 +57,8 @@ public class ChickActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 mMyChickView.setPower(progress);
+                mUtils.setVolume(progress);
+                mUtils.playMusic(progress);
             }
 
             @Override
@@ -42,7 +73,26 @@ public class ChickActivity extends AppCompatActivity {
         });
     }
 
-    void dataInit() {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mUtils.isPlaying()) {
+            mUtils.stopMusic();
+        }
+    }
 
+    @SuppressLint("HandlerLeak")
+    void dataInit() {
+        mAudioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        mUtils = new Utils(this, mAudioManager, mHandler);
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 0) {
+                    defaultVolume = mUtils.getVolume();
+                }
+            }
+        };
+        mHandler.sendEmptyMessageDelayed(0, 0);
     }
 }
--- app/src/main/java/com/chk/oocrow/Utils.java
+++ app/src/main/java/com/chk/oocrow/Utils.java
@@ -1,8 +1,11 @@
 package com.chk.oocrow;
 
 import android.content.Context;
+import android.database.ContentObserver;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Handler;
+import android.util.Log;
 
 /**
  * Created by ljc on 17-12-27.
@@ -12,37 +15,76 @@ public class Utils {
     private AudioManager mAudioManager;
     private MediaPlayer mMediaPlayer;
     private float mSpeed;
+    private Context mContext;
+    private SettingsContentObserver mSettingsContentObserver;
+    private Handler mHandler;
 
-    public Utils(Context context) {
-        mAudioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+    public Utils(Context context, AudioManager audioManager, Handler handler) {
+        mContext = context;
+        mHandler = handler;
+        mAudioManager = audioManager;
         mMediaPlayer = MediaPlayer.create(context, R.raw.crownew);
-        mMediaPlayer.start();
+        mMediaPlayer.setLooping(true);
+        registerVolumeChangeReceiver(mContext, mHandler);
     }
 
-    public int getVolume(){
+    public int getVolume() {
         int mVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
         return mVolume;
     }
 
-    public void setVolume(int volume){
-        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+    public void setVolume(int progress) {
+        float ratio = Math.round(progress / 10) / 10f;
+        mMediaPlayer.setVolume(ratio, ratio);
     }
 
-    public void playMusic(float speed){
-        mSpeed = speed;
-        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(mSpeed));
-                mMediaPlayer.start();
-            }
-        });
+    public void playMusic(int progress) {
+        if (progress > 60) {
+            mSpeed = (float) progress / 60f;
+        } else {
+            mSpeed = 1f;
+        }
         mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(mSpeed));
+        mMediaPlayer.start();
     }
 
-    public void stopMusic(){
+    public boolean isPlaying() {
+        if (mMediaPlayer == null) {
+            return false;
+        }
+        if (mMediaPlayer.isPlaying())
+            return true;
+        return false;
+    }
+
+    public void stopMusic() {
         mMediaPlayer.stop();
         mMediaPlayer.release();
+        unregisterVolumeChangeReceiver(mContext);
     }
 
+    private void registerVolumeChangeReceiver(Context context, Handler handler) {
+        mSettingsContentObserver = new SettingsContentObserver(handler);
+        context.getContentResolver().registerContentObserver(android.provider.Settings.System.CONTENT_URI, true, mSettingsContentObserver);
+    }
+
+    private void unregisterVolumeChangeReceiver(Context context) {
+        context.getContentResolver().unregisterContentObserver(mSettingsContentObserver);
+    }
+
+
+    class SettingsContentObserver extends ContentObserver {
+        private Handler mHandler;
+
+        public SettingsContentObserver(Handler handler) {
+            super(handler);
+            mHandler = handler;
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            mHandler.sendEmptyMessageDelayed(0, 0);
+        }
+    }
 }
